ddfd78f233b033391b0d5fdd1123926acb7dd9c8
Abe Levitan <abe@abelevitan.com>
2015-07-09 17:16:25 -0700

    Upgrade simple to output like a bazillion figures

--- simple.py
+++ simple.py
@@ -7,9 +7,9 @@ from sim import Sim
 xs = np.linspace(-5,5,101)
 ys = np.linspace(-5,5,101)
 Xs,Ys = np.meshgrid(xs,ys)
-fcc = np.logical_and((np.sqrt(Xs**2 + Ys**2) <= 2.5),
-                     (np.sqrt(Xs**2 + Ys**2) > 2)).astype(float)
-hcp = (np.sqrt(Xs**2 + Ys**2) <2).astype(float)
+fcc_jet = np.logical_and((np.sqrt(Xs**2 + Ys**2) <= 2.5),
+                     (np.sqrt(Xs**2 + Ys**2) >= 2)).astype(float)
+hcp_jet = (np.sqrt(Xs**2 + Ys**2) <2).astype(float)
 
 
 beam_xs = np.linspace(-75,75,1101)
@@ -19,8 +19,13 @@ beam = np.exp(-(beam_Xs**2 + beam_Zs**2)/(2*(20/2.355)**2))
 wide_beam = np.exp(-(beam_Xs**2 + beam_Zs**2)/(2*(50/2.355)**2))
 double_beam = 0.9*beam + 0.09*wide_beam + 0.01
 
+p.figure()
+p.pcolor(Xs,Ys,fcc_jet + 0.5*hcp_jet)
+p.xlabel('X distance (um)')
+p.ylabel('Y distance (um)')
+p.title('Generic H2 Jet Structure')
 
-sim = Sim((fcc,hcp),double_beam)
+sim = Sim((fcc_jet,hcp_jet),double_beam)
 
 
 fccs,hcps = [],[]
@@ -33,7 +38,7 @@ for offset in offsets:
 fccs = np.array(fccs)
 hcps = np.array(hcps)
 
-
+p.figure()
 p.plot(beam_xs,double_beam[:,550],'b-')
 p.plot(beam_xs,0.9*beam[:,550],'g--')
 p.plot(beam_xs,0.1*wide_beam[:,550],'r--')
@@ -51,11 +56,18 @@ p.xlabel('FEL offset from jet center (um)')
 p.ylabel('Contribution to scattering (arbitrary units)')
 p.legend(['FCC','HCP'])
 
+
+crude_approx = 0*offsets+np.sum(fcc_jet)/np.sum(hcp_jet) \
+               + np.diff(double_beam[550,:],n=2)[50:-49]*20 \
+               / double_beam[550,51:-50]
 p.figure()
-p.plot(0.1*offsets, fccs/hcps)
+p.plot(0.1*offsets, fccs/hcps,'b-')
+p.plot(0.1*offsets,0*offsets+np.sum(fcc_jet)/np.sum(hcp_jet), 'g--')
+p.plot(0.1*offsets, crude_approx, 'r--')
 p.title('Relative contributions (FCC/HCP) with 0.5um FCC sheath')
 p.xlabel('FEL offset from jet center (um)')
 p.ylabel('Ratio of FCC to HCP contribution')
+p.legend(['Simulated ratio','Volume ratio','Crude approximation'],loc=4) 
 
 
 
